62767fe2c023f68f5455c50b43a9abbc74bc39aa
mmi-mamun <muzahidulislam735@gmail.com>
2023-01-14 20:32:00 +0600

    Available seats counter function done

--- index.js
+++ index.js
@@ -12,22 +12,45 @@ app.use(cors());
 // MongoDB connection
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ijax3zt.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         const bookingOptionCollection = client.db('busAffinity').collection('bookingOptions');
         const bookingsCollection = client.db('busAffinity').collection('bookings');
+
+        // use aggregate to query multiple collection and then marge data
         app.get('/bookingOptions', async (req, res) => {
+            const date = req.query.date;
+            // console.log(date);
             const query = {};
             const options = await bookingOptionCollection.find(query).toArray();
+
+            /****** Get the bookings of the provided date ******/
+            /* check booking date with selected date (not clear)*/
+            const bookingQuery = { bookingDate: date };
+            /* check booked quantity on this day */
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+            console.log(alreadyBooked)
+
+            // check each card or bus
+            options.forEach(bus => {
+                /* check already booked with 6 cards via destination*/
+                const optionBooked = alreadyBooked.filter(book => book.destination === bus.destination);
+                // console.log(optionBooked);
+                const bookedSeats = optionBooked.map(book => book.seat);
+                const remainingSeats = bus.seats.filter(seat => !bookedSeats.includes(seat))
+                // console.log(date, bus.destination, bookedSeats, remainingSeats.length);
+                bus.seats = remainingSeats;
+            })
+
             res.send(options);
         })
 
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
-            console.log(booking);
+            // console.log(booking);
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
