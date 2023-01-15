8ba0ddf1e382814989cc5305f0a3a2a3c056d2f1
mmi-mamun <muzahidulislam735@gmail.com>
2023-01-15 15:26:31 +0600

    Store booking data in the table for each user

--- index.js
+++ index.js
@@ -69,6 +69,14 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            // console.log(email);
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
     }
     finally {
 
